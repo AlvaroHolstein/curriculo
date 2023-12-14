b79607dac38a3c38d3938609b8e694825b4bbafb
Alvaro Holstein <adholstein@gmail.com>
2023-12-14 00:15:17 +0000

    upgrade vue to version 3, still missing some packages to upgrade

--- curriculo_frontend/vue.config.js
+++ curriculo_frontend/vue.config.js
@@ -1,10 +1,27 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: 'pt',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: true
-    }
-  }
-}
+      locale: "pt",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: true,
+    },
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set("vue", "@vue/compat");
+
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2,
+            },
+          },
+        };
+      });
+  },
+};
